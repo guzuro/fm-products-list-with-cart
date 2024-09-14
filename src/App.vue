619e3cbdf304868d3260bbed206fa7d6f8750675
guzuro <guzurogmn@gmail.com>
2024-09-14 19:06:36 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque aspernatur excepturi
+      eligendi, laudantium at suscipit architecto deleniti reprehenderit sapiente veritatis eius
+      dolorem corporis aliquid nam ad eveniet harum ipsum ratione.
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+}
+</style>
